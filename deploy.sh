00abadad223e8d5b23a817b91b89fc7104fe2275
lzxzz <1042183935@qq.com>
2022-09-27 20:14:21 +0800

    修改readme

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,6 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m "update" &&
-git branch -M master &&
-git remote add origin git@gitee.com:frankfang/gulu-ui-website-1.git &&
-git push -f -u origin master &&
-cd -
-echo
+git branch -M main &&
+git remote add origin git@github.com:fxjzz/lzx-ui.git &&
+git push -f -u origin main &&
