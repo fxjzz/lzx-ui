f9091882e3b4dd506bd4f65f00d5c140327c09c6
lzxzz <1042183935@qq.com>
2022-11-19 21:26:03 +0800

    解决 overflow hidden 问题

--- src/lib/Popover.vue
+++ src/lib/Popover.vue
@@ -1,37 +1,62 @@
 <template>
-  <div class="popover" @click="xxx">
-    <div class="content-wrapper" v-if="visible">
+  <div class="popover" @click.stop="xxx">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
       <slot name="content"/>
     </div>
-    <slot name="x"/>
+    <span ref="triggerWrapper">
+      <slot name="x"/>
+    </span>
   </div>
 </template>
 
 <script lang="ts">
+
 export default {
-  data(){
-    return { visible:false}
+  data() {
+    return {
+      visible: false
+    };
   },
-  methods:{
-    xxx(){
-      this.visible=!this.visible
+  mounted() {
+
+  },
+  methods: {
+    xxx() {
+      this.visible = !this.visible;
+      if (this.visible === true) {
+        setTimeout(() => {
+          document.body.appendChild(this.$refs.contentWrapper);
+          const {top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
+          console.log(this.$refs.triggerWrapper.getBoundingClientRect());
+          this.$refs.contentWrapper.style.left = left +window.scrollX+ 'px';
+          this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
+          let eventHandle = () => {
+            this.visible = false;
+            document.removeEventListener('click', eventHandle);
+          };
+          document.addEventListener('click', eventHandle);
+        });
+      }
     }
   }
-}
+
+};
 </script>
 
 <style lang="scss" scoped>
-.popover{
+.popover {
   margin-top: 100px;
   display: inline-block;
   vertical-align: top;
   position: relative;
-  .content-wrapper{
-    position:absolute;
-    bottom:100%;
-    left: 0;
-    border:1px solid red;
-    box-shadow: 0 0 3px rgba(0,0,0,0.5);
-  }
+
+}
+
+.content-wrapper {
+  position: absolute;
+  bottom: 100%;
+  left: 0;
+  border: 1px solid red;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
 }
 </style>
