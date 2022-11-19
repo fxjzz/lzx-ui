a29ea2840c5e74d1805f880af7e2094638b93ea7
lzxzz <1042183935@qq.com>
2022-11-19 23:00:14 +0800

    重构 pop

--- src/lib/Popover.vue
+++ src/lib/Popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="popover" @click="xxx">
+  <div class="popover" @click="onClick">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"/>
     </div>
@@ -21,28 +21,35 @@ export default {
 
   },
   methods: {
-    xxx(event) {
+    positionContent(){
+      let {top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
+      this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+      this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
+    },
+    listenToDocument(){
+      let eventHandle = (e) => {
+        if (!(this.$refs.contentWrapper && this.$refs.contentWrapper.contains(e.target))) {
+          console.log('点击了pop以外的地方 并 关闭pop');
+          this.visible = false;
+          document.removeEventListener('click', eventHandle);
+        }
+      };
+      document.addEventListener('click', eventHandle);
+    },
+    onShow(){
+      setTimeout(() => {
+        document.body.appendChild(this.$refs.contentWrapper);//在body中添加 pop框 防止用户使用overflow 产生bug
+        this.positionContent()
+        this.listenToDocument()
+      })
+    },
+    onClick(event) {
       if (this.$refs.triggerWrapper.contains(event.target)) {
         console.log('按了按钮');
         this.visible = !this.visible;
         if (this.visible === true) {
           console.log('显示了');
-          setTimeout(() => {
-            document.body.appendChild(this.$refs.contentWrapper);
-            let {top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
-            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
-            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
-            let eventHandle = (e) => {
-              if (this.$refs.contentWrapper&&this.$refs.contentWrapper.contains(e.target)) {
-                console.log('点击了pop');
-              } else {
-                console.log('点击了pop以外的地方 并 关闭pop');
-                this.visible = false;
-                document.removeEventListener('click', eventHandle);
-              }
-            };
-            document.addEventListener('click', eventHandle);
-          });
+          this.onShow()
         }else {
           console.log('没显示');
         }
